d22f85cbdffc690e45badd9ce16edd06834bb076
kylin60 <759147983@qq.com>
2017-01-08 01:39:42 +0800

    update

---
+++ money/app/src/main/java/main/app/develop/money/MainActivity.java
@@ -0,0 +1,67 @@
+﻿package main.app.develop.money;
+
+import android.accessibilityservice.AccessibilityService;
+import android.accessibilityservice.AccessibilityServiceInfo;
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.accessibility.AccessibilityEvent;
+import android.view.accessibility.AccessibilityManager;
+import android.view.accessibility.AccessibilityNodeInfo;
+import android.widget.Button;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private final Intent mAccessibleIntent = new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS);
+    private Button switchPlugin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        switchPlugin = (Button) findViewById(R.id.button_accessible);
+        switchPlugin.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                startActivity(mAccessibleIntent);
+            }
+        });
+        updateServiceStatus();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        updateServiceStatus();
+    }
+
+    private void updateServiceStatus() {
+        boolean serviceEnabled = false;
+        AccessibilityManager accessibilityManager = (AccessibilityManager) getSystemService(Context.ACCESSIBILITY_SERVICE);
+        List<AccessibilityServiceInfo> accessibilityServices = accessibilityManager.getEnabledAccessibilityServiceList(AccessibilityServiceInfo.FEEDBACK_GENERIC);
+        for (AccessibilityServiceInfo info : accessibilityServices) {
+            if (info.getId().equals(getPackageName() + "/.QQHongbaoService")) {
+                serviceEnabled = true;
+                break;
+            }
+        }
+        if (serviceEnabled) {
+            switchPlugin.setText("关闭插件");
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        } else {
+            switchPlugin.setText("开启插件");
+            getWindow().clearFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+        }
+    }
+}
+
+
